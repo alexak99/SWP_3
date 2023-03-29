c8e916336bbb31768876a3b0f7acadc79a1cdae2
Alexandra Kind <alexandrakind2@gmail.com>
2023-03-29 17:47:40 +0200

    aggregate 2

--- workflow/aggregate.py
+++ workflow/aggregate.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import numpy as np
 
-number = 4
-X = pd.read_csv("./SWP_3/results/stats_idxstats/ERR02460{0}_tiny.txt".format(number), sep="\t", header=None)
-data = {'sequence': X[0], 'sample1': X[2]}
-df = pd.DataFrame(data)
+def create_counts(eingabe, samp):
+    table=pd.read_table(eingabe[0], header=None)
+    data={'sequence':table[0]}
+    df=pd.DataFrame(data)
 
-sample_number = 2
-while number < 9:
-    number += 1
-    X = pd.read_csv("./SWP_3/results/stats_idxstats/ERR02460{0}_tiny.txt".format(number), sep="\t", header=None)
-    df['sample{0}'.format(sample_number)] = X[2]
-    sample_number += 1
+    for i in range(samp):
+        sampletab=pd.read_table(eingabe[i], header=None)
+        df['sample'+str[i+1]]=sampletab[2]
 
-df.drop(df.tail(1).index, inplace = True)
-df.to_csv("./SWP_3/results/stats/counts.csv", sep="\t", header=True, index=False)
+df.drop(df.tail(1).index, inplace=True) 
+df.to_csv("./results/count_table")
